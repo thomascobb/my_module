11477edba73c74259976d3b2145ee5ab33dc9eb3
Tom Cobb <tom.cobb@diamond.ac.uk>
2020-04-22 11:03:10 +0100

    Added version to cli

--- my_module/cli.py
+++ my_module/cli.py
@@ -1,10 +1,11 @@
 from argparse import ArgumentParser
 
-from my_module import HelloClass, say_hello_lots
+from my_module import HelloClass, __version__, say_hello_lots
 
 
 def main(args=None):
     parser = ArgumentParser()
+    parser.add_argument('--version', action='version', version=__version__)
     parser.add_argument("name", help="Name of the person to greet")
     parser.add_argument("--times", type=int, default=5, help="Number of times to greet")
     args = parser.parse_args(args)
