d2ca60c197b927d4fa9e708d7ebb2b5b7d5856ed
Tom Cobb <tom.cobb@diamond.ac.uk>
2020-04-22 11:37:36 +0100

    black file

--- my_module/cli.py
+++ my_module/cli.py
@@ -5,7 +5,7 @@ from my_module import HelloClass, __version__, say_hello_lots
 
 def main(args=None):
     parser = ArgumentParser()
-    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument("--version", action="version", version=__version__)
     parser.add_argument("name", help="Name of the person to greet")
     parser.add_argument("--times", type=int, default=5, help="Number of times to greet")
     args = parser.parse_args(args)
